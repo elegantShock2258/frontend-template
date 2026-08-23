d5d6c58077039ae97ef51edf7f11cf8f588b8488
elegantshock <elegantshock@gmail.com>
2026-08-24 02:21:12 +0530

    title and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import styles from "./home.module.sass";
+import { links } from "./vals/values";
+import Image from "next/image";
+
 export default function Page() {
-  return <>hi</>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="self-start w-full flex flex-col gap-4">
+        <h1 className={styles.Title}>Ayush Chadha</h1>
+        <div className="w-full h-5 flex justify-around">
+          {links.map((link, i) => (
+            <a target="_blank" className={styles.link} href={link.link} key={i}>
+              <Image src={link.icon} width={30} height={30} alt={link.name} />
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      {/* <div className='grid place-items-center h-full'> */}
+
+      {/* <MouseElement /> */}
+      {/* </div> */}
+    </div>
+  );
 }
---
+++ src/app/vals/values.ts
@@ -0,0 +1,23 @@
+export const links = [
+  {
+    name: "leetcode",
+    link: "https://leetcode.com/u/ayushchadha3301/",
+    icon: "/favicon.ico",
+  },
+  {
+    name: "github",
+    link: "https://github.com/elegantShock2258/",
+    icon: "/favicon.ico",
+  },
+
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/ayush-chadha-4814b2264/",
+    icon: "/favicon.ico",
+  },
+  {
+    name: "mail",
+    link: "mailto:ayushchadha343@gmail.com",
+    icon: "/favicon.ico",
+  },
+];
